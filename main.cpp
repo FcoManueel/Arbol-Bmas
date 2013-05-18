71c1e2c5206a36a32f9e5cd97d9cce01d8725501
eduardoacye <eduardo.acye@gmail.com>
2013-05-18 14:35:50 -0700

    primera subida

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "arbol.h"
+
+using namespace std;
+
+int main()
+{
+
+    Arbol a;
+    a.agregar(123);
+    a.agregar(33);
+    a.agregar(144);
+    a.agregar(212);
+}
